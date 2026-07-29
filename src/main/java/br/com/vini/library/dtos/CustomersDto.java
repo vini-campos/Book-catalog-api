237675dc83a828ad17c29171556f6048c7422c52
vini-campos <viniciuscampos131209@gmail.com>
2026-07-29 18:07:36 -0300

    feat: create CustomersDto with  validation (NotBlank, NotNull) and Lombok

---
+++ src/main/java/br/com/vini/library/dtos/CustomersDto.java
@@ -0,0 +1,26 @@
+package br.com.vini.library.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class CustomersDto {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotNull
+    private LocalDate birthDate;
+}
