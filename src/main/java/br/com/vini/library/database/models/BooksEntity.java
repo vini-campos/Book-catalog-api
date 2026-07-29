6aec615db3e1e1e991a4ae2b89110544cda5aada
vini-campos <viniciuscampos131209@gmail.com>
2026-07-29 18:04:18 -0300

    feat(Entities): customers && books entities with respective values

---
+++ src/main/java/br/com/vini/library/database/models/BooksEntity.java
@@ -0,0 +1,28 @@
+package br.com.vini.library.database.models;
+
+import br.com.vini.library.enums.AgeGroupEnum;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BooksEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "age_group")
+    private AgeGroupEnum ageGroup;
+
+    @Column(nullable = false, name = "is_borrowed")
+    private boolean isBorrowed;
+}
---
+++ src/main/java/br/com/vini/library/database/models/CustomersEntity.java
@@ -0,0 +1,28 @@
+package br.com.vini.library.database.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, name = "birth_date")
+    private LocalDate birthDate;
+}
