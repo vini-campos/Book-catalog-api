e25e4ffa4a9eecda4edee10ed3c9697fe16da375
vini-campos <viniciuscampos131209@gmail.com>
2026-07-30 18:52:30 -0300

    feat: new endPoint to receive the customer id and returns the response to the user

--- src/main/java/br/com/vini/library/controllers/CustomersController.java
+++ src/main/java/br/com/vini/library/controllers/CustomersController.java
@@ -1,11 +1,13 @@
 package br.com.vini.library.controllers;
 
+import br.com.vini.library.dtos.BooksDto;
 import br.com.vini.library.dtos.CustomersDto;
 import br.com.vini.library.exceptions.BadRequestException;
 import br.com.vini.library.services.CustomersService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,5 +22,10 @@ public class CustomersController {
     @ResponseStatus(HttpStatus.CREATED)
     public void registerCustomer(@Valid @RequestBody CustomersDto customersDto) throws BadRequestException {
         customersService.registerCustomer(customersDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomersDto> getById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(customersService.getById(id));
     }
 }
