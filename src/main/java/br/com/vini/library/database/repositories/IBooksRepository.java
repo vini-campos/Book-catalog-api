78f8251b0f96fb33a8d0ad1a7aa9eb658bfd8683
vini-campos <viniciuscampos131209@gmail.com>
2026-08-07 18:25:41 -0300

    fix(bug): @Transactional was missing and blocking the request of the api to delete the book

--- src/main/java/br/com/vini/library/database/repositories/IBooksRepository.java
+++ src/main/java/br/com/vini/library/database/repositories/IBooksRepository.java
@@ -3,6 +3,7 @@ package br.com.vini.library.database.repositories;
 import br.com.vini.library.database.models.BooksEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface IBooksRepository extends JpaRepository<BooksEntity, Integer> {
     Optional<BooksEntity> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
+
+    @Transactional
     void deleteByIsbn(String isbn);
 }
