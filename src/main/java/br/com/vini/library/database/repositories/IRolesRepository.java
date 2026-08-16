a18d23e3f3079476080723ede063a0bb44dcf8bf
vini-campos <viniciuscampos131209@gmail.com>
2026-08-16 10:05:28 -0300

    feat: optional method to search for customers by name

--- src/main/java/br/com/vini/library/database/repositories/IRolesRepository.java
+++ src/main/java/br/com/vini/library/database/repositories/IRolesRepository.java
@@ -4,7 +4,9 @@ import br.com.vini.library.database.models.RolesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IRolesRepository extends JpaRepository<RolesEntity, Integer> {
-
+    Optional<RolesEntity> findByName(String role);
 }
